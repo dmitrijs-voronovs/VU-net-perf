541bec5ca715b874fb90ed7ae586ee706600c17e
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-11-26 23:36:36 +0100

    a1

---
+++ a1/product-form.py
@@ -0,0 +1,50 @@
+import math
+import numpy as np
+from constants import N, K, l, b, d, states
+from utils import printItem
+
+
+def productForm(state):
+    res = 1
+    for i in range(K):
+        res *= ((l[i] * d[i]) ** state[i]) / math.factorial(state[i])
+    return res
+
+
+G = 0
+for state in states:
+    G += productForm(state)
+
+# G = 1
+
+total = 0
+probabilities = {}
+for state in states:
+    res = productForm(state) / G
+    total += res
+    printItem(f"\pi{state} = {res}")
+    probabilities[state] = res
+
+print(f"total = {total}")
+
+# Q9
+
+
+def blockingProb(n):
+    res = 0
+    remaining = N - b[n]
+    for state in states:
+        if (np.dot(list(state), b) <= remaining):
+            res += probabilities[state]
+    return 1 - res
+
+
+for i in range(4):
+    printItem(f"blockingProb({i}) = {blockingProb(i)}")
+
+blockProbRes = '''
+blockingProb(0) = 0.20438212315335313
+blockingProb(1) = 0.4549233947972031
+blockingProb(2) = 0.702079807811445
+blockingProb(3) = 0.8858049881946004
+'''
---
+++ a1/robert-kaufman.py
@@ -0,0 +1,28 @@
+from constants import K, C, l, d, b
+from utils import printItem
+
+memo = {}
+
+
+def g(c):
+    if (c == 0):
+        return 1
+    if (c < 0):
+        return 0
+    if (c in memo):
+        return memo[c]
+
+    total = sum([b[j] * l[j] * d[j] * g(c - b[j]) for j in range(K)])
+    res = 1 / c * total
+    memo[c] = res
+    return res
+
+
+G = sum([g(i) for i in range(C + 1)])
+q = {}
+for c in range(0, C+1):
+    q[c] = g(c) / G
+
+for j in range(K):
+    B = sum([q[c] for c in range(C-b[j]+1, C+1)])
+    printItem(f"B_{j+1}={B}")
